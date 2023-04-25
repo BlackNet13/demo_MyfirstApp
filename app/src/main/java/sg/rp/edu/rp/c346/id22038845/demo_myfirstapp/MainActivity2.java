c7c8dd9eebc2f141d5ef7665e1caa7ec7a6d5487
YVONNE YEN CHIN WAI <22038845@myrp.edu.sg>
2023-04-25 10:21:38 +0800

    Initial commit

---
+++ app/src/main/java/sg/rp/edu/rp/c346/id22038845/demo_myfirstapp/MainActivity2.java
@@ -0,0 +1,14 @@
+package sg.rp.edu.rp.c346.id22038845.demo_myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
